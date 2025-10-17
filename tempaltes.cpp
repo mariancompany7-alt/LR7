04e7895d86bbd7cfad325ba34fb5e393cac78d24
Marian0954 <mariancompany7@gmail.com>
2025-10-17 14:26:08 +0300

    Update tempaltes.cpp

--- tempaltes.cpp
+++ tempaltes.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
+/*template <typename T>
 T Average() {
 	const int size = 5;
 	T array[size];
@@ -37,10 +37,56 @@ T amax() {
 
 	return maxValue;
 }
+*/
 
+template <typename T>
+class Myclass {
+	T array[];
+	int size;
+public:
+	Myclass();
+	Myclass(int size);
+	~Myclass();
+	void print(int size);
+	void setSize(int size);
+	int getSize();
+};
+
+template <typename T>
+Myclass<T>::Myclass() { this->size = 0; }
+
+template <typename T>
+Myclass<T>::Myclass(int size) { 
+	this->size = size; 
+	cout << "Input size of array: ";
+	for (int i = 0; i < size; i++) {
+		cin >> array[i];
+	}
+}
+
+template <typename T>
+void Myclass<T>::print(int size) {
+	cout << "Array:" << endl;
+	for (int i = 0; i < size; i++) {
+		cout << "" << array[i];
+	}
+}
+
+template <typename T>
+void Myclass<T>::setSize(int size) {
+	this->size = size;
+}
+
+template <typename T>
+int Myclass<T>::getSize() {
+	cout << "";
+}
+
+template <typename T>
+Myclass<T>::~Myclass() {}
 
 int main() {
-	cout << "average value: " << Average<int>() << endl;
+	/*cout << "average value: " << Average<int>() << endl;
 	cout << "average value: " << Average<double>() << endl;
 	cout << "average value: " << Average<char>() << endl;
 	cout << "average value: " << Average<long>() << endl;
@@ -48,7 +94,11 @@ int main() {
 	cout << "maximum value: " << amax<int>() << endl;
 	cout << "maximum value: " << amax<double>() << endl;
 	cout << "maximum value: " << amax<char>() << endl;
-	cout << "maximum value: " << amax<long>() << endl;
+	cout << "maximum value: " << amax<long>() << endl;*/
+
+	Myclass<int> object;
+	Myclass<int>object2(3);
+	object.print(3);
 
 	return 0;
 }
